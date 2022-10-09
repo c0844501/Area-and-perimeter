491de1e7264ae60d255a9e96190758ece4ab6515
c0844501 <c0844501@mylambton.ca>
2022-10-08 22:08:32 -0400

    Final

---
+++ Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace AreaAndPerimeter
+{
+    partial class AreaAndPerimeter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDiameter = new System.Windows.Forms.TextBox();
+            this.txtArea = new System.Windows.Forms.TextBox();
+            this.txtPerimeter = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtDiameter
+            // 
+            this.txtDiameter.Location = new System.Drawing.Point(368, 69);
+            this.txtDiameter.Name = "txtDiameter";
+            this.txtDiameter.Size = new System.Drawing.Size(154, 26);
+            this.txtDiameter.TabIndex = 0;
+            // 
+            // txtArea
+            // 
+            this.txtArea.Location = new System.Drawing.Point(368, 139);
+            this.txtArea.Name = "txtArea";
+            this.txtArea.ReadOnly = true;
+            this.txtArea.Size = new System.Drawing.Size(154, 26);
+            this.txtArea.TabIndex = 1;
+            this.txtArea.TabStop = false;
+            // 
+            // txtPerimeter
+            // 
+            this.txtPerimeter.Location = new System.Drawing.Point(368, 209);
+            this.txtPerimeter.Name = "txtPerimeter";
+            this.txtPerimeter.ReadOnly = true;
+            this.txtPerimeter.Size = new System.Drawing.Size(154, 26);
+            this.txtPerimeter.TabIndex = 2;
+            this.txtPerimeter.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(246, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Diameter :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(277, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Area :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(243, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Perimeter :";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(247, 302);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(117, 42);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnExit.Location = new System.Drawing.Point(405, 302);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(117, 42);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // AreaAndPerimeter
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnExit;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPerimeter);
+            this.Controls.Add(this.txtArea);
+            this.Controls.Add(this.txtDiameter);
+            this.Name = "AreaAndPerimeter";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Area And Perimeter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDiameter;
+        private System.Windows.Forms.TextBox txtArea;
+        private System.Windows.Forms.TextBox txtPerimeter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AreaAndPerimeter
+{
+    public partial class AreaAndPerimeter : Form
+    {
+        public AreaAndPerimeter()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //closing the form
+            this.Close();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //read the value from the textbox and store it into the variable
+            double diameter = double.Parse(txtDiameter.Text);
+            double area, perimeter;
+            //PI method for the value of PI and Pow is used for the Square of the radius using the Math Library
+            //Calculate the area
+            area = Math.PI * (Math.Pow(diameter / 2, 2));
+            //Roundoff the area upto 2 Digit using the Round method 
+            area = Math.Round(area, 2);
+            //Calculate the Perimeter 
+            perimeter = Math.PI * diameter;
+            perimeter = Math.Round(perimeter, 2);
+            //Display the output in the textBox
+            txtArea.Text = area.ToString();
+            txtPerimeter.Text = perimeter.ToString();
+            //Set the focus to the textbox named txtDiameter
+            txtDiameter.Focus();
+        }
+    }
+}
